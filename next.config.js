f58a671b99f4f5e824b9aa9046d615af2a61ecde
Paiva2 <joaovitor.paiva145@hotmail.com>
2023-06-12 10:01:29 -0300

    fix/strict mode to true

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api-route.ts', 'api-route.tsx'],
   images: {
     remotePatterns: [
