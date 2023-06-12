a43e265f81ba240355614a2b03a3d24c4f121a70
Paiva2 <joaovitor.paiva145@hotmail.com>
2023-06-12 11:57:40 -0300

    fix/fixed migrations and schema

---
+++ prisma/migrations/20230612143609_added_mysql_with_docker/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE `username` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NULL,
+    `usernameWithEmail` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `username_username_key`(`username`),
+    UNIQUE INDEX `username_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refreshToken` TEXT NULL,
+    `acessToken` TEXT NULL,
+    `expiresAt` INTEGER NULL,
+    `tokenType` TEXT NULL,
+    `scope` TEXT NULL,
+    `idToken` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+
+    INDEX `accounts_userId_idx`(`userId`),
+    UNIQUE INDEX `accounts_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    UNIQUE INDEX `Session_accessToken_key`(`accessToken`),
+    INDEX `Session_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
